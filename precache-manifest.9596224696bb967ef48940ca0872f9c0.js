166fd6a11005aff1ca41e0dd718ab03845c94bd8
Igi Mojares <igi.mojares@gmail.com>
2020-07-02 21:12:14 +0800

    Updates

---
+++ precache-manifest.9596224696bb967ef48940ca0872f9c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ce19a348fd9672236dc6412f3cb451e",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "daf3858127e4cdd28dc2",
+    "url": "/home/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "259c9c07c573a93ec8b0",
+    "url": "/home/static/js/2.f15713ea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/home/static/js/2.f15713ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "daf3858127e4cdd28dc2",
+    "url": "/home/static/js/main.d8860b97.chunk.js"
+  },
+  {
+    "revision": "193fad2dc12fe934bab5",
+    "url": "/home/static/js/runtime-main.55ecb4c8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/home/static/media/logo.5d5d9eef.svg"
+  }
+]);
